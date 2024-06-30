58ecd5ae19d453246a3025f8bf79399dfe02e09e
Jothei <joel.theiler@outlook.com>
2024-06-30 18:56:14 +0000

    Update JavaDoc
    Bitbucket Commit:
    
    02a5f341ad9350e01155937ae121d0158c13f978

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,19 @@ import ch.joth.games.pocketsimon.game.MainController;
 import ch.joth.games.pocketsimon.game.MainModel;
 import ch.joth.games.pocketsimon.game.MainView;
 
+/**
+ * The Main class is the entry point of the application.
+ * It creates instances of the MainModel, MainView, and MainController classes,
+ * and sets the view to be visible.
+ */
 public class Main {
+    /**
+     * The main method is the entry point of the application.
+     * It creates instances of the MainModel, MainView, and MainController classes,
+     * and sets the view to be visible.
+     *
+     * @param args command-line arguments passed to the application. This application does not use command-line arguments.
+     */
     public static void main(String[] args) {
         MainModel model = new MainModel();
         MainView view = new MainView();
